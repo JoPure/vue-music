bb6041a0dc7a8e1c79f971bb971b91e72f3cf7ca
jo <460000786@qq.com>
2018-03-15 14:40:51 +0800

    vue-music
    vue-cli仿照网易云音乐，

---
+++ src/page/home/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="my_music">
+    <div class="typelist">
+      <i class="info icon-music">
+        {{mymusic_1}}
+      </i>
+      <div class="typelist-detail">
+         <span class="name">我的音乐</span>
+         <span class="count">(28)</span>
+      </div>
+    </div>
+    </div>
+</template>
+<script type="text/ecmascript-6">
+  import axios from 'axios'
+  import {mapState} from 'vuex'
+  export default {
+    data() {
+      return {
+        mymusic_1:'我的音乐'
+      }
+    },
+    mounted() {
+      this.$nextTick(() => {
+      })
+    },
+    methods: {},
+    filters: {},
+    computed: {
+      ...mapState([])
+    }
+  }
+</script>
+<style lang="scss" type="text/scss">
+    @import '../../assets/css/base';
+
+  .typelist{
+    margin-top: pr(25);
+    height: pr(20);
+    position: relative;
+    background: rgb(247, 247, 247);
+  .typelist-detail{
+      height: 100%;
+      line-height: pr(50);
+      margin-left: pr(50);
+      box-sizing: border-box;
+      font-size: 0;
+  .count{
+    color: #aaa;
+    font-size: pr(10);
+    font-weight: 400;
+    vertical-align: middle;
+    }
+  }
+}
+</style>
